72035bad07cfeb5e8f0a9db4294edd0b9e11511a
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-01 15:32:35 +0530

    Rename site homepage to php - Check login from cookie and set variables if logged in

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'global/php/common_libs.php';
+require_once __DIR__.'/global/php/common_libs.php';
 
 $cookiedata= get_from_cookie();
 //if cookies contain anything, verify and redirect
