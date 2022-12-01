d8d21385b08df04e0a119ca39e06339acf238a79
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-01 13:46:09 +0530

    Updates to common libs - Fix: make login cookies accessible throughout domain - Add function to extract data from cookie - Add function to check username and password is valid or not i.e. authentication

--- src/global/php/common_libs.php
+++ src/global/php/common_libs.php
@@ -56,10 +56,86 @@ function user_exists($uname,$type) {
 // accepts username,password and account type and store in cookie
 function store_in_cookie($username,$password,$type)
 {
-    setcookie("username",$username,0,"","",true,false);
-    setcookie("password",$password,0,"","",true,false);
-    setcookie("type",$type,0,"","",true,false);
+    setcookie("username",$username,0,"/","",true,false);
+    setcookie("password",$password,0,"/","",true,false);
+    setcookie("type",$type,0,"/","",true,false);
 }
+
+// extract login info from cookie if present,
+// returns associative array consisting of username,type,password if present, null if not present
+function get_from_cookie() {
+    if(isset($_COOKIE['username']))
+    {
+        $result['username'] = $_COOKIE['username'];
+    }
+    else    
+        return null;
+    
+    if(isset($_COOKIE['type']))
+    {
+        $result['type'] = $_COOKIE['type'];
+    }
+    else
+        return null;
+
+    if(isset($_COOKIE['password']))
+    {
+        $result['password'] = $_COOKIE['password'];
+    }
+    else
+        return null;
+    
+    return $result;
+}
+
+// accepts username,password and account type and returns true if password is valid
+function login_authenticate($username,$password,$type) {
+    $db= connect_db();
+
+    //determine table type
+    switch($type)
+    {
+    case 'employer':
+        $table='EMPLOYER';
+        break;
+    case 'freelancer':
+        $table='FREELANCER';
+        break;
+    default:
+        die('Invalid account type');
+    }
+
+    if(user_exists($username,$type))
+    {
+        $query= sprintf("SELECT PASSWORD FROM %s WHERE USERNAME='%s'",$table, $db->real_escape_string($username));
+        $result= $db->query($query);
+        if($result)
+        {
+            if($row = $result->fetch_assoc())
+            {
+                //query valid       
+                if(password_verify($password, $row['PASSWORD']))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+                die('Query Error');
+        }
+        else
+            die('Query Error');
+    }
+    else
+    {
+        //user does not exists
+        return false;
+    }
+}
+    
 
 
 ?>
