af1eba7a30c095a0681afaba8cc638cb6672a0e2
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-02 22:05:15 +0530

    Apply changes from account widget button to employer homepage

--- src/employer_homepage/employer_homepage.php
+++ src/employer_homepage/employer_homepage.php
@@ -1,53 +1,38 @@
+<?php
+
+require_once __DIR__.'/../global/php/login_from_cookie.php';
+
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
     <title>Freelancers-Meetpoint Home</title>
     <!-- favicon in tab -->
-    <link href="global/assets/favicon.png" rel="icon">
-    <link href="global/style.css" rel="stylesheet">
+    <link href="/global/assets/favicon.png" rel="icon">
+    <link href="/global/style.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+    <script src="/global/account_info_widget.js" defer></script> 
   </head>
   
   <body>
     <!-- Header -->
     <header>
-      <a href="index.html">
-	<picture alt="logo">
-	  <source media="(max-width:600px)" srcset="global/assets/logo_with_name_initials.svg">
-	  <source media="(min-width:601px)" srcset="global/assets/logo_with_name.svg">
-	  <img src="global/assets/logo_with_name_initials.svg" height="45px" id="header-logo">
-	</picture>
+      <a href="/index.php">
+	    <picture alt="logo">
+	      <source media="(max-width:600px)" srcset="/global/assets/logo_with_name_initials.svg">
+	      <source media="(min-width:601px)" srcset="/global/assets/logo_with_name.svg">
+	      <img src="/global/assets/logo_with_name_initials.svg" height="45px" id="header-logo">
+	    </picture>
       </a>
 
       <!-- TODO: The following name, picture and menu items will be filled and populated by javascript -->
-      
-      <!-- Account info widget button -->
-      <button id="account-info-button" class="logged-out">
-	<img id="account-info-profile-pic" src="global/assets/unknown_person_profile_icon.svg" height="30px">
-	<!-- following is hidden when logged in -->
-	<p>Sign In</p>
-	<img id="hamburger-icon" src="global/assets/hamburger_menu_icon.svg" height="25px">
-      </button>
+      <?php require_once $_SERVER['DOCUMENT_ROOT'].'/global/php/account_info_widget.php' ?>
+    </header> 
 
-      <!-- menu -->
-      <div id="hamburger-menu" class="hidden">
-	<div id="hamburger-menu-header">
-	  <p id="hamburger-menu-account-name"></p>
-	  <button id="hamburger-menu-close">
-	    <img src="global/assets/close.svg" height="20px">
-	  </button>
-	</div>
-	<ul>
-	  <a>
-	    <li class="menu-item">
-	      <img height="20px">
-	      <span></span>
-	    </li>
-	  </a>
-      </div>
-    </header>
-    
+     
     <!-- Content -->
     <main>
       <div>
