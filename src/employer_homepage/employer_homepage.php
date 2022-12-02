97ecd85ef16e3734b06dde2828b4a7c1aa79a09c
TanviParsekar <115912428+TanviParsekar@users.noreply.github.com>
2022-12-02 15:20:05 +0530

    Create employer_homepage.php
    Here in employer-search-form, we have a search field and search button.
    In suggestions we have to fill using php. And in the last one we have used link part for posting a job.

---
+++ src/employer_homepage/employer_homepage.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Freelancers-Meetpoint Home</title>
+    <!-- favicon in tab -->
+    <link href="global/assets/favicon.png" rel="icon">
+    <link href="global/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+  
+  <body>
+    <!-- Header -->
+    <header>
+      <a href="index.html">
+	<picture alt="logo">
+	  <source media="(max-width:600px)" srcset="global/assets/logo_with_name_initials.svg">
+	  <source media="(min-width:601px)" srcset="global/assets/logo_with_name.svg">
+	  <img src="global/assets/logo_with_name_initials.svg" height="45px" id="header-logo">
+	</picture>
+      </a>
+
+      <!-- TODO: The following name, picture and menu items will be filled and populated by javascript -->
+      
+      <!-- Account info widget button -->
+      <button id="account-info-button" class="logged-out">
+	<img id="account-info-profile-pic" src="global/assets/unknown_person_profile_icon.svg" height="30px">
+	<!-- following is hidden when logged in -->
+	<p>Sign In</p>
+	<img id="hamburger-icon" src="global/assets/hamburger_menu_icon.svg" height="25px">
+      </button>
+
+      <!-- menu -->
+      <div id="hamburger-menu" class="hidden">
+	<div id="hamburger-menu-header">
+	  <p id="hamburger-menu-account-name"></p>
+	  <button id="hamburger-menu-close">
+	    <img src="global/assets/close.svg" height="20px">
+	  </button>
+	</div>
+	<ul>
+	  <a>
+	    <li class="menu-item">
+	      <img height="20px">
+	      <span></span>
+	    </li>
+	  </a>
+      </div>
+    </header>
+    
+    <!-- Content -->
+    <main>
+      <div>
+        <!-- TODO: employer_search remaining-->
+        <form id="employer-search-form" action="../employer_search/employer_search.php" method="get" >                  
+          <div id="employer-search-widget">               
+            <input type="search" id="search_field" name="search_query" placeholder="Find what you are Looking for">
+            <button type="submit" id="search_button">Search</button>  
+          </div>
+        </form>
+
+        <!--TODO:suggestions should be filled using php-->
+        <div id="suggestions">               
+        </div>
+
+        <p id='seperator'>OR</p>                         
+
+        <div id="post-a-job-message">                       
+          <a href="TODO">Post a Job</a>             
+        </div>
+
+      </div>  
+	    
+    </main>
+  </body>
+</html>
