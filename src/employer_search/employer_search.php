5abc7940379d23adf5298ac07c112832faeaeb48
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-03 17:57:02 +0530

    Employer Search Functionality - Display search results for query - Shows appropriate messages if empty query or no results
    Notes:
    - Results are unsorted
    - ratings, styling, filters remaining todo

--- src/employer_search/employer_search.php
+++ src/employer_search/employer_search.php
@@ -8,7 +8,35 @@ require_once __DIR__.'/../global/php/login_from_cookie.php';
 
 $search_query=$_GET['search-query'];
 
+require_once __DIR__.'/../global/php/common_libs.php';
+
+// TODO: Use more filters
+
+$db=connect_db();
+
+if($search_query)
+{
+    //TODO: sorting of results
+    $db_query_for_search= sprintf("SELECT USERNAME,PROFILE_PIC, NAME,PROFESSION, LOCATION, MIN_CHARGES, MAX_CHARGES
+ FROM FREELANCER WHERE
+ NAME LIKE '%%%s%%' OR
+ USERNAME LIKE '%%%s%%' OR
+ LOCATION LIKE '%%%s%%' OR
+ PROFESSION LIKE '%%%s%%' ;",
+                                  $db->real_escape_string($search_query),
+                                  $db->real_escape_string($search_query),
+                                  $db->real_escape_string($search_query),
+                                  $db->real_escape_string($search_query));
+
+    $db_search_result= $db->query($db_query_for_search);
+    if(!$db_search_result)
+        die('Query Error');
+}
+
+
 ?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -49,7 +77,49 @@ $search_query=$_GET['search-query'];
         </form>
 
         <!--TODO:employer_search_results should be filled using php-->
-        <div id="employer-search-results">               
+        <div id="employer-search-results">
+        <!-- Search results -->
+<?php if(!$search_query): ?>
+          <p class="search-error-message" id="search-query-empty">Please Enter Some Query</p>
+<?php elseif($db_search_result->num_rows == 0): ?>
+          <p class="search-error-message" id="search-query-no-results">
+          No Results Found for <span id="search-query-in-error"> <?php echo $search_query ?></span>
+          </p>
+<?php else: ?>
+  <?php for($i=0;
+            $i < $db_search_result->num_rows;
+            $i++): ?>
+    <?php
+          $row= $db_search_result->fetch_assoc();
+          //check if profile pic is there in database
+          if( $row['PROFILE_PIC'] )
+              $pic='/datadir/'.$row['PROFILE_PIC'];
+          else
+              $pic='/global/assets/unknown_person_profile_icon_grey.svg';
+    ?>
+    <!-- result card -->
+    <a href="/profile_page/profile_page.php?user=<?php echo urlencode($row['USERNAME']) ?>">
+      <div class="card search-result-card employer-search-result-card" >
+        <div class="employer-search-result-image" >
+          <img src='<?php echo $pic ?>' width="100px" height="100px" >
+        </div>
+        <div class="employer-search-result-text" >
+          <p class="employer-search-result-name result-title">
+            <?php echo $row['NAME'] ?>
+          </p>
+          <p class="employer-search-result-profession result-subtitle">
+            <?php echo $row['PROFESSION'] ?>
+          </p>
+          <p class="employer-search-result-location result-subsubtitle">
+            <?php echo $row['LOCATION'] ?>
+          </p>
+          <!-- TODO: Ratings -->
+        </div>
+    </a>
+  <?php endfor ?>
+<?php endif; ?>
+          
+
         </div>
 
       </div>  
