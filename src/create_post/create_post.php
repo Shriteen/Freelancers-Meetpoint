f21b582e1f3ac46db0f7c74916f5b4ecbbaed05c
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-04 22:26:21 +0530

    Update create_post.php to fix miscellenous things to bring it on par with other pages

--- src/create_post/create_post.php
+++ src/create_post/create_post.php
@@ -1,36 +1,6 @@
 <?php
 
-require_once __DIR__.'/../global/php/common_libs.php';
-
-$cookiedata= get_from_cookie();
-//if cookies contain anything, verify and redirect
-if($cookiedata)
-{
-    if(login_authenticate($cookiedata['username'],
-                          $cookiedata['password'],
-                          $cookiedata['type']) )
-    {
-        //redirect
-        switch($cookiedata['type'])
-        {
-        case 'employer':
-            header('Location: ../employer_homepage/employer_homepage.php',true);
-            exit;
-            break;
-        case 'freelancer':
-            header('Location: ../freelancer_homepage/freelancer_homepage.php',true);
-            exit;
-            break;
-        default:
-            die('Invalid account type');
-        }
-    }
-    else
-    {
-        header('Location: ../global/php/logout.php',true);
-        exit;
-    }
-}
+require_once __DIR__.'/../global/php/login_from_cookie.php';
 
 ?>
 
@@ -38,25 +8,30 @@ if($cookiedata)
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Login - Freelancers-Meetpoint</title>
+    <title>Create Post - Freelancers-Meetpoint</title>
     <!-- favicon in tab -->
-    <link href="../global/assets/favicon.png" rel="icon">
-    <link href="../global/style.css" rel="stylesheet">
-    <link href="../global/form_common_styles.css" rel="stylesheet">    
+    <link href="/global/assets/favicon.png" rel="icon">
+    <link href="/global/style.css" rel="stylesheet">
+     <!-- TODO: uncomment while styling
+     <link href="/global/form_common_styles.css" rel="stylesheet">
+    -->
     <link href="style.css" rel="stylesheet">
+    <script src="/global/account_info_widget.js" defer></script> 
   </head>
   
   <body>
     <!-- Header -->
     <header>
-      <a href="../index.html">
-	<picture alt="logo">
-	  <source media="(max-width:600px)" srcset="../global/assets/logo_with_name_initials.svg">
-	  <source media="(min-width:601px)" srcset="../global/assets/logo_with_name.svg">
-	  <img src="../global/assets/logo_with_name_initials.svg" height="45px" id="header-logo">
-	</picture>
+      <a href="/index.php">
+	    <picture alt="logo">
+	      <source media="(max-width:600px)" srcset="/global/assets/logo_with_name_initials.svg">
+	      <source media="(min-width:601px)" srcset="/global/assets/logo_with_name.svg">
+	      <img src="/global/assets/logo_with_name_initials.svg" height="45px" id="header-logo">
+	    </picture>
       </a>
-    </header>
+
+      <?php require_once $_SERVER['DOCUMENT_ROOT'].'/global/php/account_info_widget.php' ?>
+    </header> 
     
     <!-- Content -->
     <main>
@@ -64,37 +39,35 @@ if($cookiedata)
         <ul>
           <li>
             <!-- Name of Project -->
-	      <label for="create-post-project-name">Name of Project</label>
-	      <input type="text" id="create-post-project-name" name="project-name" maxlength="100" required>
+	        <label for="create-post-project-name">Name of Project</label>
+	        <input type="text" id="create-post-project-name" name="project-name" maxlength="100" required>
           </li>
     
           <li>
             <!-- Looking for -->
-	      <label for="create-post-skill-required">Looking for</label>
-	      <input type="text" id="create-post-skill-required" name="skill-required" maxlength="100" required>
+	        <label for="create-post-skill-required">Looking for</label>
+	        <input type="text" id="create-post-skill-required" name="skill-required" maxlength="100" required>
           </li>
 
           <li>
             <!-- Description -->
-	      <label for="create-post-description">description</label>
-	      <textarea id="create-post-description" name="project-description" required>
-            </textarea>
+	        <label for="create-post-description">description</label>
+	        <textarea id="create-post-description" name="project-description" required> </textarea>
           </li>
 
           <li id="post-type-selector">
             <input type="radio" id="bidable-true" name="bidable" value="true" required checked>
             <label for="bidable-true">Require Bids</label>
-	      <span>or</span>
-	      <input type="radio" id="bidable-false" name="direct-contact" value="false" required>
-	      <label for="bidable-false">Allow Direct Contact</label>
+	        <span>or</span>
+	        <input type="radio" id="bidable-false" name="bidable" value="false" required>
+	        <label for="bidable-false">Allow Direct Contact</label>
           </li>
 
           <li>
-            <button type="submit" id="post-project-button">Post</button>
+            <button type="submit" id="post-project-submit-button">Post</button>
           </li>
-
         </ul>
-      </form>
+      </form>      	    
     </main>
   </body>
 </html>
