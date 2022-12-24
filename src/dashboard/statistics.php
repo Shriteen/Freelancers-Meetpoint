b09a806c71f55f772134147be5fce47b7b07617f
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-24 22:07:53 +0530

    Graph Structure and style - HTML Skeleton for filling the graph in - CSS to style the graph boxes - Update global forms styles to style select tag

--- src/dashboard/statistics.php
+++ src/dashboard/statistics.php
@@ -30,6 +30,32 @@ function present() { ?>
       </div>
     </div>
 
+    <div id="graph-range-selector">
+      <label for="time-scale" >Select Period</label>  
+      <select id="time-scale">
+        <option value="years">Last 5 years</option>
+        <option value="months">This Year</option>
+        <option value="days" selected>Last week</option>  
+      </select>
+    </div>
 
+    <div id="statistics-graph-section">
+      <div id="freelancer-account-graph">
+        <p class="stat-title"> Freelancer Accounts </p>
+        <div class="stat-chart" id="stat-chart-freelancer"></div>
+      </div>
+      <div id="employer-account-graph">
+        <p class="stat-title"> Employer Accounts </p>
+        <div class="stat-chart" id="stat-chart-employer"></div>
+      </div>
+      <div id="post-graph">
+        <p class="stat-title"> Posts </p>
+        <div class="stat-chart" id="stat-chart-post"></div>
+      </div>
+      <div id="bid-graph">
+        <p class="stat-title"> Bids Made </p>
+        <div class="stat-chart" id="stat-chart-bid"></div>
+      </div>
+    </div>
     
 <?php } ?>
