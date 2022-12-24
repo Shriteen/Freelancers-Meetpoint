9a2fdc36c25ba5aa5ece12ff43a1d8a987f2137d
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-12-24 22:03:00 +0530

    Fix: Add body tag which was overlooked previously in dashboard.php

--- src/dashboard/dashboard.php
+++ src/dashboard/dashboard.php
@@ -18,13 +18,14 @@ require_once __DIR__.'/../global/php/common_libs.php';
     <link href="../libs/vanilla-js-tabs.css" rel="stylesheet">
     <link href="../libs/billboard.min.css" rel="stylesheet">     
     <link href="style.css" rel="stylesheet">
-    <script src="../libs/eins-modal.min.js" ></script>
     <script src="../libs/vanilla-js-tabs.min.js" ></script>
+    <script src="../libs/eins-modal.min.js" ></script>
     <script src="../libs/d3.min.js" ></script>
     <script src="../libs/billboard.min.js" ></script>
     <script src="script.js" defer></script>
      
   </head>
+  <body>   
 
 <?php
      if($_SERVER['REQUEST_METHOD'] == 'GET'):
@@ -78,5 +79,6 @@ require_once __DIR__.'/../global/php/common_libs.php';
      </div>
    </main>  
 <?php endif; ?>
-     
+
+  </body>             
 </html>
